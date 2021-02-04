e6620a682d185ad5f8105b0298b4de58e14fc389
Mingming Li <mingming.li.geo@gmail.com>
2021-02-04 11:50:27 -0700

    Thu, Feb  4, 2021 11:50:26 AM

--- class-exercise/EX04.c
+++ class-exercise/EX04.c
@@ -2,29 +2,45 @@
 #include<stdlib.h>
 #include<math.h>
 
+// Trapezoid method for integration
+// 4 Feb, 2021
+
+
+double f();
+
+
 int main(void)
 {
-double a,b,h,xi;
-double area;
-double x;
-int i;
-int n;
 
-a=0.0;b=1.0;
+        double a, b, h, x1, x2, area;
+        int n, i;
+	FILE *fp;
 
-//define the value of n
-n=1000;
-h=(b-a)/(n-1);
-
-area=0.0;
-for(i=1;i<=n-1;i=i+1)
+        a = 0.;
+        b = 1.;
+	fp=fopen("tmp.dat","w");
+for(n=2;n<1000;n=n+1)
 {
-	xi=a+(i-1)*h;
-	x=xi+h/2.0;
-	area+=h*(x*x*x+3*x+1);
+        h = (b-a)/(n-1);
+
+        area = 0.;
+        for(i=1; i<=n-1; i++)
+	{
+                x1 = a + (i-1)*h;
+                x2 = x1+h;
+                area = (f(x1)+f(x2)) + area;
+        }
+	area=area*h/2;
+        fprintf(fp,"%d %f\n",n,area);
 }
-printf("area=%f\n",area);
+fclose(fp);
+        return 0;
+}
 
-
-return 0;
+double f(x)
+	double x;
+{
+	double y;
+	y=pow(x,3.0)+3*x+1;
+	return y;
 }
