bdd8d281449d13e967665d4bdc29b3fbe7dd78a0
Mingming Li <mingming.li.geo@gmail.com>
2021-02-04 10:09:43 -0700

    Thu, Feb  4, 2021 10:09:43 AM

---
+++ class-exercise/EX04.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(void)
+{
+double a,b,h,xi;
+double area;
+double x;
+int i;
+int n;
+
+a=0.0;b=1.0;
+
+//define the value of n
+n=1000;
+h=(b-a)/(n-1);
+
+area=0.0;
+for(i=1;i<=n-1;i=i+1)
+{
+	xi=a+(i-1)*h;
+	x=xi+h/2.0;
+	area+=h*(x*x*x+3*x+1);
+}
+printf("area=%f\n",area);
+
+
+return 0;
+}
