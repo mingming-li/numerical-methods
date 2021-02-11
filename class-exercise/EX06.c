fd5678f98ffccbc88e7b35c654690a742ad35ddb
Mingming Li <mingming.li.geo@gmail.com>
2021-02-11 11:56:02 -0700

    Thu, Feb 11, 2021 11:56:02 AM

---
+++ class-exercise/EX06.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//function, read and write files
+
+double f();
+
+
+int main(void)
+{
+
+double a, b, h, x1, x2, area;
+int n, i,line;
+FILE *fp;
+char string[256];
+
+n=1000;
+
+fp=fopen("EX06.input.dat","r");
+
+for(line=1;line<=6;line++)
+{
+	fgets(string,256,fp);
+	sscanf(string,"%lf %lf",&a,&b);
+	h = (b-a)/(n-1);
+	area = 0.;
+	for(i=1; i<=n-1; i++)
+	{
+		x1 = a + (i-1)*h;
+		x2 = x1+h;
+		area = (f(x1)+4*f(x1+0.5*h)+f(x1+h)) + area;
+	}
+	area=area*h/6;
+	fprintf(stdout,"%.2f %.2f %.2f\n",a,b,area);
+}
+fclose(fp);
+return 0;
+}
+
+double f(x)
+	double x;
+{
+	double y;
+	y=pow(x,3.0)+3*x+1;
+	return y;
+}
