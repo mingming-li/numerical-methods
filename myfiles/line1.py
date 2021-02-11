2a47fa0ff03fb5b8332a5828c89a29a98676ea11
Mingming Li <mingming.li.geo@gmail.com>
2021-02-11 10:25:38 -0700

    Thu, Feb 11, 2021 10:25:38 AM

--- myfiles/line1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x,y=np.loadtxt("tmp.dat",unpack=True);
-plt.plot(x,y)
-plt.grid()
-plt.show()
--- myfiles/line3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x,y1,y2,y3=np.loadtxt("tmp.dat",unpack=True);
-plt.plot(x,y1,label="y1")
-plt.plot(x,y2,label="y2")
-plt.plot(x,y3,label="y3")
-plt.legend()
-plt.grid()
-plt.show()
