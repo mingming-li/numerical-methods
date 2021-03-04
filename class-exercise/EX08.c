7077d3a82127bac4a7334fcab92428a08e430da0
Mingming Li <mingming.li.geo@gmail.com>
2021-03-04 09:46:35 -0700

    Thu, Mar  4, 2021  9:46:34 AM

--- class-exercise/EX08.c
+++ class-exercise/EX08.c
@@ -9,7 +9,7 @@ int main(int argc,char **argv)
 {
 int i,n,np;
 double *x,*f;
-double xsub[2],fsub[2];
+double xsub[4],fsub[4];
 double a,fa;
 char string[256];
 char fname[256];
@@ -42,21 +42,39 @@ for(i=0;i<np;i++)
 }
 fclose(fp);
 
-fp=fopen("EX08.output.dat","w");
+fp=fopen("EX08.output3.dat","w");
 for(a=0.0;a<=3.1;a=a+0.01)
 {
 	for(i=0;i<np;i++)
 	{
 		if(x[i]>a)
 		{
-			xsub[1]=x[i];
-			xsub[0]=x[i-1];
-			fsub[1]=f[i];
-			fsub[0]=f[i-1];
-			break;
+			if(i>30)
+			{
+				xsub[0]=x[i];
+				xsub[1]=x[i-1];
+				xsub[2]=x[i-2];
+				xsub[3]=x[i-3];
+				fsub[0]=f[i];
+				fsub[1]=f[i-1];
+				fsub[2]=f[i-2];
+				fsub[3]=f[i-3];
+			}
+			else
+			{
+				xsub[1]=x[i];
+				xsub[0]=x[i-1];
+				fsub[1]=f[i];
+				fsub[0]=f[i-1];
+				xsub[2]=x[i+1];
+				fsub[2]=f[i+1];
+				xsub[3]=x[i+2];
+				fsub[3]=f[i+2];
+				break;
+			}
 		}
 	}
-	fa=interpolation(1,xsub,fsub,a);
+	fa=interpolation(3,xsub,fsub,a);
 	printf("a=%f fa=%f\n",a,fa);
 	fprintf(fp,"%f %f\n",a,fa);
 }
--- class-exercise/EX08.py
+++ class-exercise/EX08.py
@@ -5,6 +5,14 @@ x,y=np.loadtxt("EX08.output.dat",unpack=True);
 plt.plot(x,y,label="linear interp.")
 plt.plot(x,y,'.',label="linear interp.")
 
+x,y=np.loadtxt("EX08.output2.dat",unpack=True);
+plt.plot(x,y,label="quadratic interp.")
+plt.plot(x,y,'.',label="quadratic interp.")
+
+
+x,y=np.loadtxt("EX08.output3.dat",unpack=True);
+plt.plot(x,y,label="cubic interp.")
+plt.plot(x,y,'.',label="cubic interp.")
 
 
 x,y=np.loadtxt("EX08.dat",unpack=True);
